80850f753cd4955d08a329a473f7215d8f7e6493
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2020-12-14 13:10:25 +0100

    Added routes to the horizontal navbar

--- src/main/java/com/example/application/ui/horizontal/community/BlogView.java
+++ src/main/java/com/example/application/ui/horizontal/community/BlogView.java
@@ -6,7 +6,11 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.example.application.ui.MainView;
 
+<<<<<<< HEAD
 @Route(value = "blog", layout = MainView.class)
+=======
+@Route(value = "Blog", layout = MainView.class)
+>>>>>>> bd52951 (Added routes to the horizontal navbar)
 @PageTitle("Blog")
 public class BlogView extends Div {
 
