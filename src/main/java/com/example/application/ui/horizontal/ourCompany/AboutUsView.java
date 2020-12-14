ebb59ec9709020139cf6e64d22bd886289cd4b93
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2020-12-14 13:10:25 +0100

    Added routes to the horizontal navbar

--- src/main/java/com/example/application/ui/horizontal/ourCompany/AboutUsView.java
+++ src/main/java/com/example/application/ui/horizontal/ourCompany/AboutUsView.java
@@ -1,4 +1,4 @@
-package com.example.application.ui.horizontal.ourCompany;
+package com.example.application.ui.horizontal.unserunternehmen;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
@@ -6,7 +6,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.example.application.ui.MainView;
 
-@Route(value = "aboutUs", layout = MainView.class)
+@Route(value = "UeberUns", layout = MainView.class)
 @PageTitle("Über Uns")
 public class AboutUsView extends Div {
 
