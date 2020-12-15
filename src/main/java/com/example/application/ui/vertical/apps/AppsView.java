fce0b97d1ed02ba341421878dd2a0f39cb842420
Vanessa Skowronsky <vskowronsky@stud.hs-bremen.de>
2020-12-15 20:42:53 +0100

    f-Routen_setzen Revert to functioning main version

--- src/main/java/com/example/application/ui/vertical/apps/AppsView.java
+++ src/main/java/com/example/application/ui/vertical/apps/AppsView.java
@@ -1,10 +1,11 @@
 package com.example.application.ui.vertical.apps;
 
+import com.example.application.ui.MainView;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.example.application.ui.MainView;
+
 
 @Route(value = "apps", layout = MainView.class)
 @PageTitle("Apps")
