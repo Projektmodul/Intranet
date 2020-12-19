cc82b0ee224fb16046175f9067328713c8f6e00f
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2020-12-19 11:05:25 +0100

    Label und Textfeld (read only) für Vornamen hinzugefügt

--- src/main/java/com/example/application/ui/vertical/myProfile/MyProfileView.java
+++ src/main/java/com/example/application/ui/vertical/myProfile/MyProfileView.java
@@ -2,6 +2,9 @@ package com.example.application.ui.vertical.myProfile;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.example.application.ui.MainView;
@@ -12,7 +15,20 @@ public class MyProfileView extends Div {
 
     public MyProfileView() {
         setId("myProfile-view");
-        add(new Text("Content placeholder"));
+
+        VerticalLayout content = new VerticalLayout();
+        content.setSizeFull();
+
+        content.addComponentAsFirst(new Label("Mein Profil"));
+
+        TextField firstname = new TextField();
+        firstname.setValue("Test");
+        firstname.setLabel("Vorname");
+        firstname.setReadOnly(true);
+        content.addComponentAtIndex(1, firstname);
+
+
+        add(content);
     }
 
 }
