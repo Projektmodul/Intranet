50d6ea696a9b71ca0e11e056ff6431e38285df50
Anastasiya Jackwerth <ajackwerth@stud.hs-bremen.de>
2021-01-12 11:47:09 +0100

    Updated CanteenView and added a style canteen.css. Only frontend not backend. It is possible to click the image and make it bigger and exit with ESCAPE/outer click/or the x. Uploading (with a Button or with Drag and Drop) a file is already implemented but has no functions yet. Delete Button also misses a clickEvent because we did not implement backend yet. Right now we are only displaying an Image not a PDF, so there will be changes later on.

--- src/main/java/com/example/application/ui/vertical/canteen/CanteenView.java
+++ src/main/java/com/example/application/ui/vertical/canteen/CanteenView.java
@@ -1,14 +1,36 @@
 package com.example.application.ui.vertical.canteen;
 
 import com.example.application.ui.ContentHolder;
-import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.example.application.ui.MainView;
+
+/**
+ * This is a View for all menus.
+ *
+ * @author  Sabrine Gamdou, Anastasiya Jackwerth
+ * @version 1.0
+ * @since   14.12.2020
+ * @lastUpdated 12.01.2021
+ */
 
 @Route(value = "canteen", layout = ContentHolder.class)
 @PageTitle("Betriebsrestaurant")
+@CssImport("./styles/views/main/canteen.css")
 public class CanteenView extends Div {
 
     public CanteenView() {
