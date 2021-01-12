35aae517909cbcd9f4972770705e108e968ea09d
Anastasiya Jackwerth <ajackwerth@stud.hs-bremen.de>
2021-01-12 11:47:20 +0100

    Updated CanteenView and added a style canteen.css. Only frontend not backend. It is possible to click the image and make it bigger and exit with ESCAPE/outer click/or the x. Uploading (with a Button or with Drag and Drop) a file is already implemented but has no functions yet. Delete Button also misses a clickEvent because we did not implement backend yet. Right now we are only displaying an Image not a PDF, so there will be changes later on.

--- src/main/java/com/example/application/ui/vertical/canteen/CanteenView.java
+++ src/main/java/com/example/application/ui/vertical/canteen/CanteenView.java
@@ -33,10 +33,94 @@ import com.vaadin.flow.router.Route;
 @CssImport("./styles/views/main/canteen.css")
 public class CanteenView extends Div {
 
+    private Component leftComponent;
+    private Component rightComponent;
+
+    private H1 pageTitle;
+    private Span pageContent;
+    private Image pdfImage;
+    private Icon deleteIcon;
+
+    private Upload uploadButton;
+
+    private SplitLayout splitLayout;
+
+    private Dialog imageDialog;
+
     public CanteenView() {
-        setId("canteen-view");
         setClassName("pageContentPosition");
-        add(new Text("Ich habe hunger!!!"));
+
+        initializeLeftContainer();
+        initializeRightContainer();
+        initializeImageDialog();
+        initializeSplitLayout();
+    }
+
+    public void initializeLeftContainer(){
+        pageTitle = new H1("Speiseplan");
+        pageTitle.setId("pageTitle");
+
+        deleteIcon = new Icon(VaadinIcon.TRASH);
+        deleteIcon.setId("deleteIcon");
+
+        HorizontalLayout menuDeleteLayout = new HorizontalLayout(pageTitle,deleteIcon);
+        menuDeleteLayout.setId("menuDeleteLayout");
+
+        /*For frontend this is enough, later for backend this will be changed */
+        pdfImage = new Image("images/speiseplanBeispiel.png","Speiseplan-PDF");
+        pdfImage.setId("pdfImage");
+        /*-------------------------------------------------------------------*/
+
+        pageContent = new Span("Werfen Sie doch schon vorab einen Blick in unsere Speisekarte.");
+
+        leftComponent = new VerticalLayout(menuDeleteLayout,pdfImage,pageContent);
+        leftComponent.setId("leftComponent");
+    }
+
+    public void initializeRightContainer(){
+        initializeUploadButton();
+        rightComponent = new VerticalLayout(uploadButton);
+        rightComponent.setId("rightComponent");
+    }
+
+    public void initializeSplitLayout(){
+        splitLayout = new SplitLayout(leftComponent,rightComponent);
+        splitLayout.setId("splitLayout");
+        this.add(splitLayout);
+    }
+
+    public void initializeUploadButton(){
+        MemoryBuffer buffer = new MemoryBuffer();
+        uploadButton = new Upload(buffer);
+
+        uploadButton.setUploadButton(new Button("Speiseplan hochladen"));
+        uploadButton.setDropLabel(new Span("PDF-Dokument hier reinziehen"));
+
+    }
+
+    public void initializeImageDialog(){
+        Image dialogImage = new Image("images/speiseplanBeispiel.png","Speiseplan-PDF");
+        dialogImage.setId("dialogPdfImage");
+
+        imageDialog = new Dialog();
+        initializeCloseIcon();
+        imageDialog.add(dialogImage);
+
+        imageDialog.setId("imageDialog");
+        add(imageDialog);
+
+        setDialogClickEvent();
+    }
+
+    public void setDialogClickEvent(){
+        pdfImage.addClickListener(e-> imageDialog.open());
+    }
+
+    public void initializeCloseIcon(){
+        Icon closeIcon = new Icon(VaadinIcon.CLOSE);
+        closeIcon.addClickListener(e -> imageDialog.close());
+        imageDialog.add(closeIcon);
+        closeIcon.setId("closeIcon");
     }
 
 }
