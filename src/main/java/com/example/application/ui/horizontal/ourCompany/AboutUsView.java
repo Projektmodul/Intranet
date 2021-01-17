4ebdd6798a257f3b40ebcad40ae56b4b38e23fae
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-01-17 15:04:23 +0100

    AboutViewService Klasse hinzugefügt. AboutView Klasse bearbeitet, damit der Text aus der datenbank angezeigt wird

--- src/main/java/com/example/application/ui/horizontal/ourCompany/AboutUsView.java
+++ src/main/java/com/example/application/ui/horizontal/ourCompany/AboutUsView.java
@@ -1,8 +1,12 @@
 package com.example.application.ui.horizontal.ourCompany;
 
+import com.example.application.backend.entities.PageEntity;
+import com.example.application.backend.services.ourCompany.AboutUsViewService;
 import com.example.application.ui.ContentHolder;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.example.application.ui.MainView;
@@ -11,10 +15,27 @@ import com.example.application.ui.MainView;
 @PageTitle("Über Uns")
 public class AboutUsView extends Div {
 
-    public AboutUsView() {
+    private AboutUsViewService aboutUsViewService;
+
+    private H1 pageTitle;
+    private Span pageContent;
+
+    public AboutUsView(AboutUsViewService aboutUsViewService) {
+        this.aboutUsViewService = aboutUsViewService;
+
         setId("aboutUs-view");
         setClassName("pageContentPosition");
-        add(new Text("Über Uns"));
+
+        setData();
+    }
+
+    private void setData(){
+        PageEntity pageEntity = aboutUsViewService.findPageById(4);
+
+        pageTitle = new H1(pageEntity.getTitle());
+        pageContent = new Span(pageEntity.getContent());
+
+        this.add(pageTitle, pageContent);
     }
 
 }
