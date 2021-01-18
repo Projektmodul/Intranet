0345b458a60465f779d2e341faccd38d873bef87
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-01-18 01:32:07 +0100

    Created a service for the ImageEntity and the DocumentEntity.

---
+++ src/main/java/com/example/application/backend/services/images/ImageService.java
@@ -0,0 +1,30 @@
+package com.example.application.backend.services.images;
+
+import com.example.application.backend.entities.ImageEntity;
+import com.example.application.backend.repositories.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+
+    private ImageRepository imageRepository;
+
+    @Autowired
+    public ImageService(ImageRepository imageRepository){
+        this.imageRepository = imageRepository;
+    }
+
+    public ImageRepository getImageRepository(){
+        return imageRepository;
+    }
+
+    public void save(ImageEntity imageEntity){
+        getImageRepository().saveAndFlush(imageEntity);
+    }
+
+    public ImageEntity findImageById(int imageId) {
+        return getImageRepository().findByImageId(imageId);
+    }
+
+}
