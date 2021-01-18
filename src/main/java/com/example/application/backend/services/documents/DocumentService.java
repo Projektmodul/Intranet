71b1d17814c9f501749a62719f8955932750ef6b
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-01-18 01:34:16 +0100

    Removed some unnecessary imports.

--- src/main/java/com/example/application/backend/services/documents/DocumentService.java
+++ src/main/java/com/example/application/backend/services/documents/DocumentService.java
@@ -5,7 +5,7 @@ import com.example.application.backend.repositories.DocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
 
 @Service
 public class DocumentService {
