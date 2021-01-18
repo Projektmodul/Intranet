ef8617b1f4fe93df1f540b1968d2bd1f8082d210
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-01-18 01:56:05 +0100

    Added header comment with some metadata :').

--- src/main/java/com/example/application/backend/utils/PdfDocumentViewer.java
+++ src/main/java/com/example/application/backend/utils/PdfDocumentViewer.java
@@ -5,6 +5,16 @@ import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.server.StreamResource;
 
+/**
+ * A UI-Component that prints a streamResource (pdf file in this case) to the
+ *  screen in a proper pdf viewer/container.
+ *
+ * @author  Sabrine Gamdou
+ * @version 1.0
+ * @since   18.01.2021
+ * @lastUpdated  18.01.2021
+ */
+
 @Tag("object")
 public class PdfDocumentViewer extends Component implements HasSize {
 
