629411c9326a98eb9d5bbc2ae4a74428ab7512ed
Rebecca Schirmacher <rebsch11@googlemail.com>
2021-01-19 11:48:28 +0100

    added in settings the option to change the theme to dark/light mode

--- src/main/java/com/example/application/ui/vertical/settings/SettingsView.java
+++ src/main/java/com/example/application/ui/vertical/settings/SettingsView.java
@@ -2,14 +2,18 @@ package com.example.application.ui.vertical.settings;
 
 import com.example.application.ui.MainView;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.dom.ThemeList;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  *  Settings View shows ...
  *
- *  @author
+ *  @author Rebecca Schirmacher
  *  @version 1.0
  *  @since 15.12.2020
  *  @lastUpdated
@@ -22,7 +26,20 @@ public class SettingsView extends Div {
         setId("settings");
         setClassName("pageContentPosition");
         addClassName("homeColorscheme");
-        add(new Text("Content placeholder"));
+
+        add(new Text("Stellen Sie hier ein, ob Sie ein helles oder dunkles Thema haben möchten:"));
+
+        Button toggleButton = new Button("Ändere das Thema", click -> {
+            ThemeList themeList = UI.getCurrent().getElement().getThemeList(); //
+
+            if (themeList.contains(Lumo.DARK)) {
+                themeList.remove(Lumo.DARK);
+            } else {
+                themeList.add(Lumo.DARK);
+            }
+        });
+
+        add(toggleButton);
     }
 
 }
