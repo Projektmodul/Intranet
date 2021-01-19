951f2c90422818603a13de28cc5bac3604b6039b
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-01-19 08:24:44 +0100

    added find method to the ImageRepository

--- src/main/java/com/example/application/backend/repositories/ImageRepository.java
+++ src/main/java/com/example/application/backend/repositories/ImageRepository.java
@@ -4,4 +4,5 @@ import com.example.application.backend.entities.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<ImageEntity, Integer> {
+    ImageEntity findByImageId(int imageId);
 }
