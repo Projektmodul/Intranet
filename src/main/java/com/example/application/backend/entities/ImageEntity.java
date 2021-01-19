af9285773aee1e9e1f951ff9e64b4752a7432197
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-01-19 08:23:22 +0100

    updated header comment.

--- src/main/java/com/example/application/backend/entities/ImageEntity.java
+++ src/main/java/com/example/application/backend/entities/ImageEntity.java
@@ -7,9 +7,9 @@ import javax.persistence.*;
  * This is a basic image class.
  *
  * @author  Sabrine Gamdou
- * @version 1.0
+ * @version 2.0
  * @since   05.01.2020
- * @lastUpdated 05.01.2021
+ * @lastUpdated 19.01.2021
  */
 
 @Entity(name ="images")
