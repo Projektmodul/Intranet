f01327b966acf5136c6da9d4bea799a41168de72
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-01-19 08:32:58 +0100

    updated header comment.

--- src/main/java/com/example/application/backend/utils/pdfs/PdfDocumentViewer.java
+++ src/main/java/com/example/application/backend/utils/pdfs/PdfDocumentViewer.java
@@ -4,6 +4,14 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.server.StreamResource;
+/**
+ * This is a basic address class.
+ *
+ * @author  Jessica Reistel, Laura Neuendorf and Sabrine Gamdou
+ * @version 3.0
+ * @since   21-12-2020
+ * @lastUpdated 19.01.2021 from Sabrine Gamdou
+ */
 
 @Tag("object")
 public class PdfDocumentViewer extends Component implements HasSize {
