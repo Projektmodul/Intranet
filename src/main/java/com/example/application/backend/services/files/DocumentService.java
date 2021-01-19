e477510c6c8abd512d8910aa4afcf20d09b9c378
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-01-19 08:46:41 +0100

    created service class for documents.

---
+++ src/main/java/com/example/application/backend/services/files/DocumentService.java
@@ -0,0 +1,34 @@
+package com.example.application.backend.services.files;
+
+import com.example.application.backend.entities.DocumentEntity;
+import com.example.application.backend.repositories.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class DocumentService {
+
+    private DocumentRepository documentRepository;
+
+    @Autowired
+    DocumentService(DocumentRepository documentRepository){
+        this.documentRepository = documentRepository;
+    }
+
+    public DocumentRepository getDocumentRepository(){return documentRepository; }
+
+    public void save (DocumentEntity documentEntity){
+        getDocumentRepository().saveAndFlush(documentEntity);
+    }
+
+    public DocumentEntity findDocumentById(int documentId){
+        return getDocumentRepository().findByDocumentId(documentId);
+    }
+
+    public void delete (DocumentEntity documentEntity){
+        getDocumentRepository().delete(documentEntity);
+    }
+
+}
