27ccda23525fd35b8032cc1c7abe775edd32f57d
Monika Martius <mmartius@stud.hs-bremen.de>
2021-01-19 08:14:25 +0100

    * Added content for noticeboard. * An object class was created for filling the layout Objects.java

--- src/main/java/com/example/application/ui/horizontal/community/NoticeBoardView.java
+++ src/main/java/com/example/application/ui/horizontal/community/NoticeBoardView.java
@@ -1,28 +1,141 @@
 package com.example.application.ui.horizontal.community;
 
 import com.example.application.ui.MainView;
-import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *  NoticeBoard View shows ...
- *
- *  @author
+ *  @author Monika Martius
  *  @version 1.0
- *  @since 15.12.2020
- *  @lastUpdated
+ *  @since 19.01.2021
+ *  @lastUpdated 19.01.2021
  */
 @Route(value = "noticeBoard", layout = MainView.class)
 @PageTitle("Schwarzes Brett")
+
 public class NoticeBoardView extends Div {
+
+
+    //private noticeBoardService noticeBoardService;
+
+    private H1 pageTitle;
+    private Icon deleteIcon;
+    private Icon editIcon;
+    private Span pageContent;
+    private Upload uploadButton;
+    private HorizontalLayout layoutSplit;
+    private RadioButtonGroup radioOffer;
+    private Button toAdd;
+    private List<Objects> objectList = new ArrayList<>();
+
+    private Component leftComponent;
+    private Component rightComponent;
+    //private CardListNoticeBoard card;
 
     public NoticeBoardView() {
         setId("noticeBoard");
         setClassName("pageContentPosition");
         addClassName("communityColorscheme");
-        add(new Text("Schwarzes Brett"));
+
+        initializeLeftContainer();
+        initializeRightContainer();
+        initializeSplitLayout();
     }
 
+    public void initializeLeftContainer() {
+
+        pageTitle = new H1("Schwarzes Brett");
+        pageTitle.setId("pageTitle");
+
+        Div box = new Div();
+        box.add(pageTitle);
+        box.setId("layoutBox");
+
+        deleteIcon = new Icon(VaadinIcon.TRASH);
+        deleteIcon.setId("deleteIcon");
+
+        editIcon = new Icon(VaadinIcon.EDIT);
+        editIcon.setId("editIcon");
+
+        HorizontalLayout editDeleteIcon = new HorizontalLayout();
+        editDeleteIcon.add(box, editIcon, deleteIcon);
+        editDeleteIcon.setAlignItems(FlexComponent.Alignment.END);
+        editDeleteIcon.setClassName("editDeleteIcon");
+        pageContent = new Span("Wir wünschen Ihnen viel Spaß beim Suchen, Finden, Anbieten und Stöbern!");
+
+        objectList.add(new Objects(100, "Wohnung", Status.SUCHE, 450, "liliad@gmail.com"));
+        objectList.add(new Objects(101, "Wohnung", Status.BIETE, 300, "miamaia@gmail.com"));
+        objectList.add(new Objects(102, "Katze", Status.SUCHE, 0, "ldfsdfdfd@gmail.com"));
+        objectList.add(new Objects(103, "Auto", Status.SUCHE, 3000, "nnfdgfg@gmail.com"));
+        objectList.add(new Objects(104, "Wohnung", Status.SUCHE, 450, "liliad@gmail.com"));
+        objectList.add(new Objects(105, "Wohnung", Status.BIETE, 300, "miamaia@gmail.com"));
+        objectList.add(new Objects(106, "Katze", Status.SUCHE, 0, "ldfsdfdfd@gmail.com"));
+        objectList.add(new Objects(107, "Auto", Status.BIETE, 500, "nnfdgfg@gmail.com"));
+        objectList.add(new Objects(108, "Wohnung", Status.SUCHE, 450, "liliad@gmail.com"));
+        objectList.add(new Objects(109, "Wohnung", Status.BIETE, 300, "miamaia@gmail.com"));
+        objectList.add(new Objects(110, "Katze", Status.BIETE, 10, "ldfsdfdfd@gmail.com"));
+        objectList.add(new Objects(111, "Auto", Status.SUCHE, 3000, "nnfdgfg@gmail.com"));
+
+        Grid<Objects> grid = new Grid<>(Objects.class);
+        grid.setItems(objectList);
+        grid.removeColumnByKey("id");
+
+        leftComponent = new VerticalLayout(editDeleteIcon,grid,pageContent);
+        leftComponent.setId("leftLayout");
+    }
+
+    public void initializeRightContainer(){
+        initializeUploadButton();
+        initializeRadioButton();
+        TextArea textArea = new TextArea("Beschreibung");
+        textArea.getStyle().set("font-family", "Gothic A1, serif");
+        textArea.getStyle().set("minHeight", "200px");
+        textArea.getStyle().set("minWidth", "300px");
+        textArea.setPlaceholder("Anzeige aufgeben ...");
+        toAdd = new Button();
+        toAdd.getStyle().set("font-family", "Gothic A1, serif");
+        toAdd.setText("Hinzufügen");
+        toAdd.setWidth("170px");
+        rightComponent = new VerticalLayout(textArea,uploadButton,radioOffer,toAdd);
+        rightComponent.setId("rightLayout");
+    }
+
+    public void initializeSplitLayout(){
+        layoutSplit = new HorizontalLayout(leftComponent,rightComponent);
+        layoutSplit.setId("splitLayout");
+        this.add(layoutSplit);
+    }
+
+    public void initializeUploadButton(){
+        MemoryBuffer buffer = new MemoryBuffer();
+        uploadButton = new Upload(buffer);
+        uploadButton.setUploadButton(new Button("Bild hochladen"));
+        uploadButton.setDropLabel(new Span("Bild-Datei hier reinziehen"));
+    }
+
+    public void initializeRadioButton(){
+        radioOffer = new RadioButtonGroup();
+        radioOffer.setLabel("Was möchten Sie?");
+        radioOffer.getStyle().set("color", "948B21");
+        radioOffer.getStyle().set("font-family", "Gothic A1, serif");
+        radioOffer.setItems("Bieten", "Suchen");
+    }
 }
