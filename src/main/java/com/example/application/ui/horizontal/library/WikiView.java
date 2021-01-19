dead531fcd942f4bb879a8ef0a725ccede95561d
Rebecca Schirmacher <rebsch11@googlemail.com>
2021-01-19 11:36:08 +0100

    added the links in BlogView and WikiView which open new tabs

--- src/main/java/com/example/application/ui/horizontal/library/WikiView.java
+++ src/main/java/com/example/application/ui/horizontal/library/WikiView.java
@@ -2,14 +2,16 @@ package com.example.application.ui.horizontal.library;
 
 import com.example.application.ui.MainView;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 /**
  *  Wiki View shows ...
  *
- *  @author
+ *  @author Rebecca Schirmacher
  *  @version 1.0
  *  @since 15.12.2020
  *  @lastUpdated
@@ -21,6 +23,14 @@ public class WikiView extends Div {
         setId("wiki");
         setClassName("pageContentPosition");
         addClassName("libraryColorscheme");
-        add(new Text("Wiki"));
+
+        VerticalLayout verticalLayout = new VerticalLayout();
+
+        //link has to be updated with the actual bsag wiki address
+        Anchor linkWiki = new Anchor("https://de.wikipedia.org/wiki/Bremer_Stra%C3%9Fenbahn_AG", "Wiki");
+        linkWiki.setTarget("https://de.wikipedia.org/wiki/Bremer_Stra%C3%9Fenbahn_AG");
+
+        verticalLayout.add(linkWiki);
+        add(verticalLayout);
     }
 }
