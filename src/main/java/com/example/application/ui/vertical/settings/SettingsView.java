dd6ad08ee048793f3a026001b7acce86f196eb46
Rebecca Schirmacher <rebsch11@googlemail.com>
2021-01-19 17:14:05 +0100

    changed the button for light/dark mode to a switch in SettingsView

--- src/main/java/com/example/application/ui/vertical/settings/SettingsView.java
+++ src/main/java/com/example/application/ui/vertical/settings/SettingsView.java
@@ -1,15 +1,19 @@
 package com.example.application.ui.vertical.settings;
 
 import com.example.application.ui.MainView;
+import com.vaadin.componentfactory.ToggleButton;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.dom.ThemeList;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  *  Settings View shows ...
  *
- *  @author
+ *  @author Rebecca Schirmacher
  *  @version 1.0
  *  @since 15.12.2020
  *  @lastUpdated
@@ -22,7 +26,20 @@ public class SettingsView extends Div {
         setId("settings");
         setClassName("pageContentPosition");
         addClassName("homeColorscheme");
-        add(new Text("Content placeholder"));
+        add(new Text("Stellen Sie hier ein helles oder dunkles Farbschema ein:"));
+
+        ToggleButton toggleButton = new ToggleButton();
+        toggleButton.addClickListener(event -> toggleDarkMode());
+        add(toggleButton);
     }
 
+    private void toggleDarkMode(){
+        ThemeList themeList = UI.getCurrent().getElement().getThemeList();
+
+        if (themeList.contains(Lumo.DARK)) {
+            themeList.remove(Lumo.DARK);
+        } else {
+            themeList.add(Lumo.DARK);
+        }
+    }
 }
