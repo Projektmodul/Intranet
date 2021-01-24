bc79665eed7f1170decfdf051ff961acf0bcdfe0
Monika Martius <mmartius@stud.hs-bremen.de>
2021-01-24 16:33:52 +0100

    NoticeBoardView erstellt und den Service hinzugefügt sowie die Entity der neuen DB Klasse angepasst. Das NoticeBoardView wird aus der DB gelesen!

--- src/main/java/com/example/application/backend/repositories/NoticeBoardOfferRepository.java
+++ src/main/java/com/example/application/backend/repositories/NoticeBoardOfferRepository.java
@@ -1,7 +1,17 @@
+/**
+ *  Home View shows ...
+ *
+ *  @author Monika Martius, Jessica Reistel
+ *  @version 1.0
+ *  @since 23.01.2021
+ *  @lastUpdated 23.01.2021
+ */
 package com.example.application.backend.repositories;
 
 import com.example.application.backend.entities.NoticeBoardOfferEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeBoardOfferRepository extends JpaRepository<NoticeBoardOfferEntity, Integer> {
+    NoticeBoardOfferEntity findByNoticeBoardOfferId(int noticeBoardOfferId);
+
 }
