2d4fba2cf7b97069681ac2bb7016242598eba9a0
Monika Martius <mmartius@stud.hs-bremen.de>
2021-01-24 16:22:41 +0100

    Anpassen der PageID und einlesen des Titels und des contents aus der Datenbank den vertikalen Seiten

--- src/main/java/com/example/application/ui/vertical/help/HelpView.java
+++ src/main/java/com/example/application/ui/vertical/help/HelpView.java
@@ -1,8 +1,12 @@
 package com.example.application.ui.vertical.help;
 
+import com.example.application.backend.entities.PageEntity;
+import com.example.application.backend.services.pages.PageService;
 import com.example.application.ui.MainView;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -17,13 +21,20 @@ import com.vaadin.flow.router.Route;
 @Route(value = "help", layout = MainView.class)
 @PageTitle("Hilfe")
 public class HelpView extends Div {
+    private PageService pageService;
+    private H1 pageTitle;
+    private H2 pageText;
+    private PageEntity pageEntity;
 
-    public HelpView() {
+    public HelpView(PageService pageService) {
         setId("help");
         setClassName("pageContentPosition");
         addClassName("homeColorscheme");
-        add(new Text("Content placeholder"));
 
+        pageEntity = pageService.findPageById(25);
+        pageTitle = new H1(pageEntity.getTitle());
+        pageText = new H2(pageEntity.getContent());
+
+        add(pageTitle);
     }
-
 }
