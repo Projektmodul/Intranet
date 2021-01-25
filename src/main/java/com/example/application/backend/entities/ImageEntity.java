10f4a562d40710abf534fa1b7a207c3170b72050
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-01-25 15:02:32 +0100

    Kommentarbereich aktualisiert

--- src/main/java/com/example/application/backend/entities/ImageEntity.java
+++ src/main/java/com/example/application/backend/entities/ImageEntity.java
@@ -6,10 +6,10 @@ import javax.persistence.*;
 /**
  * This is a basic image class.
  *
- * @author  Sabrine Gamdou
+ * @author  Sabrine Gamdou, Jessica Reistel, Monika Martius and Laura Neuendorf
  * @version 2.0
  * @since   05.01.2020
- * @lastUpdated 19.01.2021
+ * @lastUpdated 25.01.2021
  */
 
 @Entity(name ="images")
