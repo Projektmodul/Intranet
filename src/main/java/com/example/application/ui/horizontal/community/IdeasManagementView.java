97aff03e3791f9342cb3c6063074864f19d10f36
Jessica Reistel <jreistel@stud.hs-bremen.de>
2021-01-25 22:16:26 +0100

    Schleife zum Auslesen der Ideen hinzugefügt. Teilweise Layout und Namen angepasst. Code teilweise in Methoden ausgelagert.

--- src/main/java/com/example/application/ui/horizontal/community/IdeasManagementView.java
+++ src/main/java/com/example/application/ui/horizontal/community/IdeasManagementView.java
@@ -18,15 +18,14 @@ import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- *  IdeaManagement View shows ...
+ *  IdeaManagement View shows all ideas from the database
  *
  *  @author Litharshiga Sivarasa, Jessica Reistel
- *  @version 1.0
+ *  @version 2.0
  *  @since 15.12.2020
  *  @lastUpdated 25.01.2021
  */
@@ -34,104 +33,100 @@ import java.util.List;
 @PageTitle("Ideenmanagement")
 public class IdeasManagementView extends Div {
 
-    private PageService pageService;
-    private IdeaService ideaService;
-    private PageEntity pageEntity;
-    private IdeaEntity ideaEntity;
-    private IdeaEntity ideaEntity1;
-    private IdeaEntity ideaEntity2;
+    private final IdeaService ideaService;
 
     public IdeasManagementView(PageService pageService, IdeaService ideaService) {
-        this.pageService = pageService;
         this.ideaService = ideaService;
 
         setId("ideasManagement");
         setClassName("pageContentPosition");
         addClassName("communityColorscheme");
 
-        pageEntity = pageService.findPageById(21);
-
+        PageEntity pageEntity = pageService.findPageById(21);
         H1 title = new H1(pageEntity.getTitle());
 
-        ideaEntity = ideaService.findById(1);
-        ideaEntity1 = ideaService.findById(2);
-        ideaEntity2 = ideaService.findById(3);
-        List<IdeaEntity> ideaList = Arrays.asList(ideaEntity, ideaEntity1, ideaEntity2);
-
-
-        /*
-         *Create a grid bound to the list
-         */
-        Grid<IdeaEntity> grid = new Grid<>();
-        grid.setItems(ideaList);
-        grid.addColumn(IdeaEntity::getTitle).setHeader("Überschrift");
-        grid.addColumn(IdeaEntity::getDescription).setHeader("Idee");
-        grid.addColumn(IdeaEntity::getRating).setHeader("Status");
-        grid.addThemeVariants(GridVariant.LUMO_COLUMN_BORDERS,GridVariant.LUMO_ROW_STRIPES,GridVariant.LUMO_WRAP_CELL_CONTENT);
-        grid.getStyle().set("border", "2px solid #a6a6a6");
-        grid.setSelectionMode(Grid.SelectionMode.NONE);
-        grid.setId("tableIdea");
-
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-
         Button buttonIdea = new Button("Idee einreichen", new Icon(VaadinIcon.LIGHTBULB));
-        buttonIdea.addClickListener(e -> initContentDialog().open());
+        buttonIdea.addClickListener(e -> initDialogAdd().open());
         buttonIdea.setIconAfterText(true);
 
-        Label ideas = new Label(pageEntity.getContent());
-        ideas.setClassName("quote");
+        Paragraph content = new Paragraph(pageEntity.getContent());
 
-        horizontalLayout.add(ideas, buttonIdea);
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.add(content, buttonIdea);
 
-        add(title, horizontalLayout, grid);
+        add(title, horizontalLayout, initializeGrid());
+    }
+
+    /**
+     * The method initialize the Grid with all ideas
+     * @return initializeGrid
+     */
+    public Grid initializeGrid(){
+        List<IdeaEntity> ideaList = new ArrayList<>();
+        int maxId = ideaService.findMaxId();
+        IdeaEntity ideaEntity;
+        for(int i=1 ; i<=maxId ; i++){
+            if(ideaService.findById(i) != null){
+                ideaEntity = ideaService.findById(i);
+                ideaList.add(ideaEntity);
+            }
+        }
+
+        Grid<IdeaEntity> grid = new Grid<>();
+        grid.setItems(ideaList);
+        grid.setSelectionMode(Grid.SelectionMode.NONE);
+        grid.addColumn(IdeaEntity::getTitle, "Titel").setHeader("Titel");
+        grid.addColumn(IdeaEntity::getDescription, "Idee").setHeader("Idee");
+        grid.addColumn(IdeaEntity::getRating, "Status").setHeader("Status");
+        grid.addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
+        grid.setId("tableIdea");
+
+        return grid;
     }
 
     /*
-     * The method initContentDialog generates a dialog window
-     * @return contentDialog
+     * The method initDialogAdd generates a dialog window to add an idea
+     * @return addDialog
      */
-    private Dialog initContentDialog() {
-        Dialog contentDialog = new Dialog();
-        contentDialog.setCloseOnOutsideClick(false);
-        contentDialog.setCloseOnEsc(false);
+    private Dialog initDialogAdd() {
+        Dialog addDialog = new Dialog();
+        addDialog.setCloseOnOutsideClick(false);
+        addDialog.setCloseOnEsc(false);
 
         Div saveCancel = new Div();
         saveCancel.setId("saveCancelDiv");
-
         Button saveButton = new Button("Idee einreichen");
-        Button cancelButton = new Button("Abbrechen", e -> contentDialog.close());
+        Button cancelButton = new Button("Abbrechen", e -> addDialog.close());
         saveButton.addClassName("ideaButton");
         cancelButton.addClassName("ideaButton");
         saveCancel.add(saveButton, cancelButton);
 
         HorizontalLayout update = new HorizontalLayout();
+        update.addComponentAsFirst(ideaAdd());
+        addDialog.add(new H3("Ideen einreichen"), update, saveCancel);
 
-
-        update.addComponentAsFirst(ideaUpdate());
-        contentDialog.add(new H3("Ideen einreichen"), update, saveCancel);
-
-        return contentDialog;
+        return addDialog;
     }
 
     /*
-     * The method ideaUpdate generates the vertical layout for idea update
-     * @return updateIdea
+     * The method ideaAdd generates the vertical layout to add a idea
+     * @return addIdea
      */
-    private VerticalLayout ideaUpdate() {
-        VerticalLayout updateIdea = new VerticalLayout();
+    private VerticalLayout ideaAdd() {
+        VerticalLayout addIdea = new VerticalLayout();
 
-        TextField updateTitle = new TextField();
-        updateTitle.setValue(ideaEntity.getTitle());
-        updateTitle.setLabel("Überschrift");
+        TextField newTitle = new TextField();
+        newTitle.setPlaceholder("Ideetitel");
+        newTitle.setLabel("Titel");
 
-        TextArea updateDescription = new TextArea();
-        updateDescription.setValue(ideaEntity.getDescription());
-        updateDescription.setLabel("Idee");
+        TextArea newDescription = new TextArea();
+        newDescription.setPlaceholder("Beschreibung");
+        newDescription.setLabel("Idee");
 
-        updateIdea.addComponentAsFirst(updateTitle);
-        updateIdea.addComponentAtIndex(1, updateDescription);
+        addIdea.addComponentAsFirst(newTitle);
+        addIdea.addComponentAtIndex(1, newDescription);
 
-        return updateIdea;
+        return addIdea;
     }
 
 }
