2ab91b3df61554d6ee3fd953d0abbe1294d5aa7d
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-01-26 11:50:08 +0100

    Initiator to trigger an event, when the notification counter is changed.

---
+++ src/main/java/com/example/application/ui/vertical/notifications/NotificationCounterChangedInitiator.java
@@ -0,0 +1,30 @@
+package com.example.application.ui.vertical.notifications;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Initiator to trigger an event, when the notification counter is changed
+ *
+ *
+ * @author  Anastasiya Jackwerth, Sabrine Gamdou
+ * @version 1.0
+ * @since   26.01.2021
+ * @lastUpdated 26.01.2021
+ */
+public class NotificationCounterChangedInitiator {
+    private List<NotificationCounterChangedListener> listeners = new ArrayList<>();
+
+    public void addListener(NotificationCounterChangedListener notificationCounterChangedListener){
+        listeners.add(notificationCounterChangedListener);
+    }
+
+    public void notificationCounterChanged(){
+        for(NotificationCounterChangedListener listener : listeners){
+            listener.notificationCounterChanged();
+        }
+    }
+
+
+
+}
