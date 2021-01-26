5e2db3729d6f6f59533438414e297ad1394b80e3
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-01-26 11:57:58 +0100

    Added a notification counter and a delete button. Changed texts that were in english.

--- src/main/java/com/example/application/ui/vertical/notifications/NotificationsView.java
+++ src/main/java/com/example/application/ui/vertical/notifications/NotificationsView.java
@@ -2,21 +2,20 @@ package com.example.application.ui.vertical.notifications;
 
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
+
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H4;
-
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 /**
  * Notification Dialog, pops up when the Bulb/Notification icon is clicked.
  *
  * @author  Sabrine Gamdou
- * @version 1.0
+ * @version 2.0
  * @since   20-12-2020
- * @lastUpdated 16.01.2021
+ * @lastUpdated 26.01.2021 from Anastasiya Jackwerth, Sabrine Gamdou
  */
 
 @CssImport("./styles/views/main/notification.css")
@@ -25,19 +24,27 @@ public class NotificationsView extends Dialog {
 
     private H4 notificationText ;
     private VerticalLayout allNotificationsContainer;
+    private NativeButton deleteButton;
+
+    private int notificationsCounter;
 
 
     public NotificationsView() {
         setId("notifications");
 
-        notificationText = new H4("Notifications: ");
+        notificationText = new H4("Ihre Benachrichtigungen: ");
 
         allNotificationsContainer = new VerticalLayout(notificationText);
+
+        deleteButton = new NativeButton("Löschen");
+        deleteButton.setId("deleteButton");
+        allNotificationsContainer.add(deleteButton);
 
         this.add(allNotificationsContainer);
         allNotificationsContainer.addClassName("notificationsContainer");
 
-        Button closeButton = new Button("Close", event -> this.close());
+        Button closeButton = new Button("Schließen", event -> this.close());
+
 
         closeButton.setId("notificationBtn");
         allNotificationsContainer.add(closeButton);
@@ -49,5 +56,15 @@ public class NotificationsView extends Dialog {
         return allNotificationsContainer;
     }
 
+    public NativeButton getDeleteButton() {
+        return deleteButton;
+    }
 
+    public int getNotificationsCounter() {
+        return notificationsCounter;
+    }
+
+    public void setNotificationsCounter(int notificationsCounter) {
+        this.notificationsCounter = notificationsCounter;
+    }
 }
