7427e86d79b3d359eb8c04ba35fed812e364bd6c
Jessica Reistel <jreistel@stud.hs-bremen.de>
2021-01-26 21:22:09 +0100

    HelpView überarbeitet, sodass sie mit der Datenbank verbunden ist.

--- src/main/java/com/example/application/ui/vertical/help/HelpView.java
+++ src/main/java/com/example/application/ui/vertical/help/HelpView.java
@@ -1,40 +1,60 @@
 package com.example.application.ui.vertical.help;
 
+import com.example.application.backend.entities.LinkEntity;
 import com.example.application.backend.entities.PageEntity;
+import com.example.application.backend.services.links.LinkService;
 import com.example.application.backend.services.pages.PageService;
 import com.example.application.ui.MainView;
-import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
+import com.example.application.ui.horizontal.library.FAQView;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 
 /**
  *  Help View shows ...
  *
- *  @author
- *  @version 1.0
+ *  @author Jessica Reistel
+ *  @version 2.0
  *  @since 15.12.2020
- *  @lastUpdated
+ *  @lastUpdated 26.01.2021
  */
 @Route(value = "help", layout = MainView.class)
 @PageTitle("Hilfe")
 public class HelpView extends Div {
-    private PageService pageService;
-    private H1 pageTitle;
-    private H2 pageText;
-    private PageEntity pageEntity;
 
-    public HelpView(PageService pageService) {
+    private PageService pageService;
+    private LinkService linkService;
+    private H1 pageTitle;
+    private Paragraph pageContent;
+    private HorizontalLayout links;
+
+    public HelpView(PageService pageService, LinkService linkService) {
+        this.pageService = pageService;
+        this.linkService = linkService;
+
         setId("help");
         setClassName("pageContentPosition");
         addClassName("homeColorscheme");
 
-        pageEntity = pageService.findPageById(25);
-        pageTitle = new H1(pageEntity.getTitle());
-        pageText = new H2(pageEntity.getContent());
+        setContent();
 
-        add(pageTitle);
+        add(pageTitle, pageContent, links);
+    }
+
+    private void setContent(){
+        PageEntity pageEntity = pageService.findPageById(25);
+        pageTitle = new H1(pageEntity.getTitle());
+        pageContent = new Paragraph(pageEntity.getContent());
+        pageContent.getElement().setProperty("innerHTML", pageEntity.getContent());
+
+        LinkEntity linkEntity = linkService.findById(3);
+        Anchor mailLink = new Anchor(linkEntity.getUrl(), linkEntity.getTitle());
+
+        RouterLink faqLink = new RouterLink("Antworten auf häufig gestellte Fragen", FAQView.class);
+
+        links = new HorizontalLayout();
+        links.add(faqLink, mailLink);
     }
 }
