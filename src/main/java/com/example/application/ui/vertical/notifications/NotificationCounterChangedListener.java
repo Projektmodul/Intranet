cf6cceb25d950a08e4b02cc6b5974ef4d8ae3dde
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-01-26 11:51:20 +0100

    Added an interface to implement a change listener.

---
+++ src/main/java/com/example/application/ui/vertical/notifications/NotificationCounterChangedListener.java
@@ -0,0 +1,14 @@
+package com.example.application.ui.vertical.notifications;
+
+/**
+ * Interface to implement a change listener
+ *
+ *
+ * @author  Anastasiya Jackwerth, Sabrine Gamdou
+ * @version 1.0
+ * @since   26.01.2021
+ * @lastUpdated 26.01.2021
+ */
+public interface NotificationCounterChangedListener {
+    void notificationCounterChanged();
+}
