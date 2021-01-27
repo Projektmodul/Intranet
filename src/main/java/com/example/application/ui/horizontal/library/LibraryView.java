68a3d2be7dddb75963614bfdc8b84a65b39e733e
Jessica Reistel <jreistel@stud.hs-bremen.de>
2021-01-27 13:20:30 +0100

    pageText zu firstQuote geändert. secondQuote von Label zu H2 geändert. Styleklasse firstQuote wieder eingefügt. Codestruktur geändert

--- src/main/java/com/example/application/ui/horizontal/library/LibraryView.java
+++ src/main/java/com/example/application/ui/horizontal/library/LibraryView.java
@@ -7,10 +7,7 @@ import com.vaadin.componentfactory.Breadcrumb;
 import com.vaadin.componentfactory.Breadcrumbs;
 import com.example.application.ui.auxiliary.OverviewComponents;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -21,9 +18,9 @@ import com.vaadin.flow.router.Route;
  *  Library View shows ...
  *
  *  @author Litharshi Sivarasa, Vanessa Skowronsky
- *  @version 2.0
+ *  @version 3.0
  *  @since 15.12.2020
- *  @lastUpdated 25.01.2021 by Vanessa Skowronsky
+ *  @lastUpdated 27.01.2021 by Jessica Reistel
  */
 @Route(value = "library", layout = MainView.class)
 @PageTitle("Bibliothek")
@@ -32,25 +29,34 @@ public class LibraryView extends Div {
 
     private PageService pageService;
     private H1 pageTitle;
-    private H2 pageText;
-    private PageEntity pageEntity;
+    private H2 firstQuote;
+    private H2 secondQuote;
+    private HorizontalLayout layout;
 
     public LibraryView(PageService pageService) {
+        this.pageService = pageService;
         setId("library");
         setClassName("pageContentPosition");
         addClassName("libraryColorscheme");
 
-        pageEntity = pageService.findPageById(13);
-        pageTitle = new H1(pageEntity.getTitle());
-        pageText = new H2(pageEntity.getContent());
-
         Breadcrumbs breadcrumbs = new Breadcrumbs();
         breadcrumbs.add(new Breadcrumb("Home"), new Breadcrumb(pageEntity.getTitle()));
 
-        Label secondQuote = new Label("entdecken");
+        setContent();
+
+        add(pageTitle, firstQuote, secondQuote, layout);
+    }
+
+    private void setContent(){
+        PageEntity pageEntity = pageService.findPageById(13);
+
+        pageTitle = new H1(pageEntity.getTitle());
+        firstQuote = new H2(pageEntity.getContent());
+        secondQuote = new H2("entdecken");
+        firstQuote.setClassName("firstQuote");
         secondQuote.setClassName("secondQuote");
 
-        HorizontalLayout layout = new HorizontalLayout();
+        layout = new HorizontalLayout();
         layout.setPadding(true);
         layout.addClassName("justifyContentCenter");
 
@@ -67,7 +73,7 @@ public class LibraryView extends Div {
         layout.add(componentDocuments, componentWiki, componentArchive, componentMedia, componentFAQ);
 
 
-        add(pageTitle,pageText,secondQuote, layout);
+
 
     }
     }
--- src/main/java/com/example/application/ui/horizontal/ourCompany/OurCompanyView.java
+++ src/main/java/com/example/application/ui/horizontal/ourCompany/OurCompanyView.java
@@ -10,7 +10,7 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Label;
+
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -21,34 +21,45 @@ import com.vaadin.flow.router.Route;
  *  OurCompany View shows ...
  *
  *  @author Litharshi Sivarasa, Vanessa Skowronsky
- *  @version 2.0
+ *  @version 3.0
  *  @since 15.12.2020
- *  @lastUpdated 25.01.2021 by Vanessa Skowronsky
+ *  @lastUpdated 27.01.2021 by Jessica Reistel
  */
 @Route(value = "ourCompany", layout = MainView.class)
 @PageTitle("Unser Unternehmen")
 public class OurCompanyView extends Div {
+
     private PageService pageService;
     private H1 pageTitle;
-    private H2 pageText;
-    private PageEntity pageEntity;
+    private H2 firstQuote;
+    private H2 secondQuote;
+    private HorizontalLayout layout;
 
     public OurCompanyView(PageService pageService) {
-        setId("ourCompany");
-        setClassName("pageContentPosition");
-        addClassName("ourCompanyColorscheme");
+        this.pageService = pageService;
 
-        pageEntity = pageService.findPageById(3);
-        pageTitle = new H1(pageEntity.getTitle());
-        pageText = new H2(pageEntity.getContent());
+        setId("centers");
+        setClassName("pageContentPosition");
+        addClassName("centerColorscheme");
 
         Breadcrumbs breadcrumbs = new Breadcrumbs();
         breadcrumbs.add(new Breadcrumb("Home"), new Breadcrumb(pageEntity.getTitle()));
 
-        Label secondQuote = new Label("für sie da");
+        setContent();
+
+        add(breadcrumbs,pageTitle, firstQuote, secondQuote, layout);
+    }
+
+    private void setContent(){
+        PageEntity pageEntity = pageService.findPageById(3);
+
+        pageTitle = new H1(pageEntity.getTitle());
+        firstQuote = new H2(pageEntity.getContent());
+        secondQuote = new H2("für sie da");
+        firstQuote.setClassName("firstQuote");
         secondQuote.setClassName("secondQuote");
 
-        HorizontalLayout layout = new HorizontalLayout();
+        layout = new HorizontalLayout();
         layout.setPadding(true);
         layout.addClassName("justifyContentCenter");
 
@@ -57,9 +68,9 @@ public class OurCompanyView extends Div {
         Component componentNews = OverviewComponents.createComponent(new Icon(VaadinIcon.NEWSPAPER), "#A00505", "Nachrichten", "news");
         Component componentSport = OverviewComponents.createComponent(new Icon(VaadinIcon.EXIT), "#A00505", "Sport & Freizeit", "sport");
         Component componentCareer = OverviewComponents.createComponent(new Icon(VaadinIcon.DOLLAR), "#A00505", "Stellenangebote", "career");
+
         layout.add(componentWelcome, componentAboutUs, componentNews, componentSport, componentCareer);
 
-        add(breadcrumbs, pageTitle, pageText, secondQuote, layout);
     }
 }
 
