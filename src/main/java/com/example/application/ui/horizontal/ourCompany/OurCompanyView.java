aae825ee09b02ab31e09bd2aeea0f301b22cf22b
Jessica Reistel <jreistel@stud.hs-bremen.de>
2021-01-27 13:30:31 +0100

    Copy-Paste-Fehler rückgängig gemacht.

--- src/main/java/com/example/application/ui/horizontal/ourCompany/OurCompanyView.java
+++ src/main/java/com/example/application/ui/horizontal/ourCompany/OurCompanyView.java
@@ -35,9 +35,9 @@ public class OurCompanyView extends Div {
     public OurCompanyView(PageService pageService) {
         this.pageService = pageService;
 
-        setId("centers");
+        setId("ourCompany");
         setClassName("pageContentPosition");
-        addClassName("centerColorscheme");
+        addClassName("ourCompanyColorscheme");
 
         setContent();
 
