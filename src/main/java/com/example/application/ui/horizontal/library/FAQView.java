e3ffe8da9dcc6045d077b7051c5d2887e77c847f
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-01-28 08:57:25 +0100

    Die Methode setData() wurde der FAQVIew hinzugefügt. Die Daten werden nun aus der DB ausgelesen

--- src/main/java/com/example/application/ui/horizontal/library/FAQView.java
+++ src/main/java/com/example/application/ui/horizontal/library/FAQView.java
@@ -1,39 +1,57 @@
 package com.example.application.ui.horizontal.library;
 
+import com.example.application.backend.entities.LinkEntity;
 import com.example.application.backend.entities.PageEntity;
+import com.example.application.backend.services.links.LinkService;
 import com.example.application.backend.services.pages.PageService;
 import com.example.application.ui.MainView;
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 /**
  *  FAQ View shows ...
  *
- *  @author Monika Martius
- *  @version 1.0
+ *  @author Monika Martius and Laura Neuendorf
+ *  @version 2.0
  *  @since 15.12.2020
- *  @lastUpdated 22.01.2021
+ *  @lastUpdated 27.01.2021 by Laura Neuendorf
  */
 @Route(value = "fAQ", layout = MainView.class)
 @PageTitle("FAQ")
 public class FAQView extends Div {
     private PageService pageService;
-    private H1 pageTitle;
-    private H2 pageText;
     private PageEntity pageEntity;
 
-    public FAQView(PageService pageService) {
+    private LinkService linkService;
+
+    private H1 pageTitle;
+    private Paragraph pageContent;
+
+
+    public FAQView(PageService pageService, LinkService linkService) {
         setId("fAQ");
         setClassName("pageContentPosition");
         addClassName("libraryColorscheme");
 
+        this.pageService = pageService;
+        this.linkService = linkService;
+
+        setData();
+
+    }
+
+    public void setData(){
         pageEntity = pageService.findPageById(17);
         pageTitle = new H1(pageEntity.getTitle());
-        pageText = new H2(pageEntity.getContent());
-        add(pageTitle);
+        pageContent = new Paragraph();
+        pageContent.setText(pageEntity.getContent());
+        pageContent.getElement().setProperty("innerHTML", pageEntity.getContent());
+
+        LinkEntity linkEntity = linkService.findById(4);
+        Anchor mailLink = new Anchor(linkEntity.getUrl(), linkEntity.getTitle());
+
+        this.add(pageTitle, pageContent, mailLink);
     }
 }
