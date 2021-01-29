0be18b5b32ff432496c02c89688d691a92774f7d
Vanessa Skowronsky <vskowronsky@stud.hs-bremen.de>
2021-01-29 15:11:22 +0100

    Rebased commit; minor fix in CareerView

--- src/main/java/com/example/application/ui/horizontal/ourCompany/CareerView.java
+++ src/main/java/com/example/application/ui/horizontal/ourCompany/CareerView.java
@@ -1,19 +1,16 @@
 package com.example.application.ui.horizontal.ourCompany;
 
+import com.example.application.backend.entities.JobOfferEntity;
 import com.example.application.backend.entities.PageEntity;
 import com.example.application.backend.services.pages.PageService;
-import com.example.application.backend.entities.JobOfferEntity;
 import com.example.application.ui.MainView;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -64,9 +61,6 @@ public class CareerView extends Div {
     }
 
     public void initializeLeftContainer(){
-        H1 careerH = new H1("Stellenangebote");
-        careerH.setId("careerHeadL");
-
         Label quoteCareer = new Label("Nutzen Sie die Chance und bewerben Sie sich online bei uns. ");
         quoteCareer.setClassName("quoteCareer");
 
@@ -101,10 +95,7 @@ public class CareerView extends Div {
         Label pageContent = new Label("Zur Unterstützung unseres Teams suchen wir immer wieder ideenreiche und ehrgeizige Kolleginnen und Kollegen. Deshalb freuen wir uns auch über Initiativbewerbungen.");
         pageContent.setClassName("pageContent");
 
-        HorizontalLayout menuLayout = new HorizontalLayout(careerH);
-        menuLayout.setId("menuLayout");
-
-        leftComponent = new VerticalLayout(menuLayout,quoteCareer,iconAndOfferDiv,grid, pageContent);
+        leftComponent = new VerticalLayout(quoteCareer,iconAndOfferDiv,grid, pageContent);
         leftComponent.setId("leftComponent");
     }
 
