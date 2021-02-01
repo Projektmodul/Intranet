af97241406e2b0c0e299783c02caa41fd780ba8a
Jessica Reistel <jreistel@stud.hs-bremen.de>
2021-02-01 19:05:54 +0100

    Abstände angepasst und Icon hinzugefügt

--- src/main/java/com/example/application/ui/InProgressView.java
+++ src/main/java/com/example/application/ui/InProgressView.java
@@ -2,10 +2,11 @@ package com.example.application.ui;
 
 import com.example.application.backend.entities.PageEntity;
 import com.example.application.backend.services.pages.PageService;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -13,9 +14,9 @@ import com.vaadin.flow.router.Route;
  *  InProgress View is used for sides that have no functionality. It is a fallback class with an informative text.
  *
  *  @author Vanessa Skowronsky
- *  @version 1.0
+ *  @version 2.0
  *  @since 04.01.2021
- *  @lastUpdated 01.02.2021 by Vanessa Skowronsky
+ *  @lastUpdated 01.02.2021 by Jessica Reistel
  */
 @Route(value = "inProgress", layout = MainView.class)
 @PageTitle("Im Aufbau")
@@ -33,7 +34,11 @@ public class InProgressView extends Div {
         pageEntity = pageService.findPageById(2);
         pageTitle = new H1(pageEntity.getTitle());
         pageText = new H2(pageEntity.getContent());
+        pageText.setId("inProgressH2");
 
-        add(pageTitle, pageText);
+        Icon icon = new Icon(VaadinIcon.TOOLS);
+        Div iconDiv = new Div(icon);
+
+        add(pageTitle, pageText, iconDiv);
     }
 }
