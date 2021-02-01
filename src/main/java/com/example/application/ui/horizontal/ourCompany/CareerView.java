c284d8453682bbedef96aea4433b97a3c56fa0a3
Anastasiya Jackwerth <ajackwerth@stud.hs-bremen.de>
2021-02-01 16:35:57 +0100

    See previous commit in the CareerView file

--- src/main/java/com/example/application/ui/horizontal/ourCompany/CareerView.java
+++ src/main/java/com/example/application/ui/horizontal/ourCompany/CareerView.java
@@ -48,61 +48,87 @@ import java.util.List;
 @Route(value = "career", layout = MainView.class)
 @PageTitle("Stellenangebote")
 public class CareerView extends Div {
+
     private PageService pageService;
     private H1 pageTitle;
     private Paragraph pageText;
     private PageEntity pageEntity;
+
     private Component leftComponent;
     private Component rightComponent;
-    private Upload uploadButton;
+
     private Button toAdd;
     private Button toDelete;
+
     private SplitLayout splitLayout;
 
     private Grid<GridJobOffer> jobOfferGrid;
-    private DocumentService documentService;
+    private Div pdfsUploader;
+    private ComboBox<String> typeDropBox;
+    private ComboBox<String> categoryDropBox;
+
     private NotificationService notificationService;
 
-    private Div pdfsUploader;
-    private String keyword;
+    TextField description;
+    TextField location;
+    Div errorContainer;
+    Div addClearDiv;
+
+    private boolean isDescriptionFilled = false;
+    private boolean isTypeFilled = false;
+    private boolean isCategoryFilled = false;
+    private boolean isLocationFilled = false;
 
     private PdfsManager pdfsManager;
     private UserService userService;
     private UserEntity userEntity;
+    private DocumentService documentService;
     private JobOfferService jobOfferService;
     private JobOfferEntity jobOfferEntity;
 
 
     public CareerView(PageService pageService, DocumentService documentService,
-                      JobOfferService jobOfferService) {
+                      JobOfferService jobOfferService, UserService userService, NotificationService notificationService) {
         this.pageService = pageService;
         this.jobOfferService = jobOfferService;
         this.documentService = documentService;
+        this.userService = userService;
+        this.notificationService = notificationService;
 
         setId("career");
         setClassName("pageContentPosition");
         addClassName("ourCompanyColorscheme");
 
-        int maxId = jobOfferService.findMaxId();
-        pageEntity = pageService.findPageById(8);
-        pageTitle = new H1(pageEntity.getTitle());
-        pageText = new Paragraph(pageEntity.getContent());
+        setData();
 
         Breadcrumbs breadcrumbs = new Breadcrumbs();
         breadcrumbs.add(new Breadcrumb("Home"), new Breadcrumb("Unser Unternehmen"), new Breadcrumb(pageEntity.getTitle()));
 
-        DocumentEntity documentManagement = new DocumentEntity();
-        documentManagement.setFileName("Verwaltung-Dokument");
-        documentManagement.setKeyword("Verwaltung");
 
         add(breadcrumbs, pageTitle, pageText);
 
-        initializeTreeGrid();
         initializePdfsManager();
         initializeUploadContainer();
+        initializeGrid();
+
         initializeLeftContainer();
         initializeRightContainer();
+
         initializeSplitLayout();
+    }
+
+    public void setData(){
+        pageEntity = pageService.findPageById(8);
+        pageTitle = new H1(pageEntity.getTitle());
+        pageText = new Paragraph(pageEntity.getContent());
+        jobOfferEntity = new JobOfferEntity();
+
+        GetUserController getUserController = new GetUserController();
+        String username = getUserController.getUsername();
+        userEntity = userService.findByUsername(username);
+
+        errorContainer = new Div();
+        errorContainer.setText("Bitte laden Sie erst eine PDF Datei hoch und füllen Sie anschließend die nachfologenden Felder aus.");
     }
 
     public void initializeLeftContainer(){
@@ -123,16 +149,19 @@ public class CareerView extends Div {
         leftComponent.setId("leftComponent");
     }
 
-    public void initializeTreeGrid(){
+    public void initializeGrid(){
         jobOfferGrid = new Grid<>();
+
         jobOfferGrid.setItems(GridJobOffer.JobOfferEntitiesConverter.convertJobOfferEntities(pageEntity.getJobOffers()));
         jobOfferGrid.setSelectionMode(Grid.SelectionMode.NONE);
+
         jobOfferGrid.addColumn(GridJobOffer::getTitle, "Bezeichnung").setHeader("Bezeichnung");
         jobOfferGrid.addColumn(GridJobOffer::getCategory, "Kategorie").setHeader("Kategorie");
         jobOfferGrid.addColumn(GridJobOffer::getType,"Stellenart").setHeader("Stellenart");
         jobOfferGrid.addColumn(GridJobOffer::getLocation, "Arbeitsort").setHeader("Arbeitsort");
-        jobOfferGrid.addColumn(GridJobOffer::getKeyword, "keyword").setHeader("Abteilung");
+
         jobOfferGrid.addColumn(new ComponentRenderer<>(GridJobOffer::getDownloadLink)).setHeader("Dateiname");
+
         jobOfferGrid.addThemeVariants(GridVariant.LUMO_COLUMN_BORDERS,GridVariant.LUMO_ROW_STRIPES,GridVariant.LUMO_WRAP_CELL_CONTENT);
         jobOfferGrid.getStyle().set("border", "2px solid #a6a6a6");
         jobOfferGrid.setSelectionMode(Grid.SelectionMode.NONE);
@@ -151,17 +180,6 @@ public class CareerView extends Div {
         rightComponent.setId("rightComponent");
     }
 
-    public void initializeUploadButton(){
-        MemoryBuffer buffer = new MemoryBuffer();
-        uploadButton = new Upload(buffer);
-
-        uploadButton.setUploadButton(new Button("Stellenangebot hochladen"));
-        uploadButton.setDropLabel(new Span("PDF-Dokument hier reinziehen"));
-
-    }
-
-
-
     public void initializeSplitLayout(){
         splitLayout = new SplitLayout(leftComponent,rightComponent);
         splitLayout.setId("splitLayout");
@@ -169,20 +187,126 @@ public class CareerView extends Div {
     }
 
     public void initializePdfsManager(){
-        pdfsManager = new PdfsManager(pageEntity.getDocuments(),notificationService,documentService);
+        List<DocumentEntity> tempDocumentsList = initializeJobOfferDocuments();
+        pdfsManager = new PdfsManager(tempDocumentsList,notificationService,documentService);
 
-        pdfsManager.setDocumentEntities(pageEntity.getDocuments());
-        pdfsManager.setAllDocumentEntitiesData(keyword,pageEntity,pageEntity.getUser());
+        pdfsManager.setDocumentEntities(tempDocumentsList);
+        pdfsManager.setAllDocumentEntitiesData("Verwaltung",pageEntity,pageEntity.getUser());
         pdfsManager.setOnePdf(false);
 
         pdfsManager.initializeAllPdfs();
     }
 
     public void initializeUploadContainer(){
-        pdfsManager.initializeUploadContainer();
+        pdfsManager.initializeUploadContainerForJobOffers();
         pdfsUploader = pdfsManager.getPdfsUploader();
+    }
 
-        this.add(pdfsUploader);
+    public List<DocumentEntity> initializeJobOfferDocuments(){
+        List<DocumentEntity> tempDocumentsList = new ArrayList<>();
+        for(JobOfferEntity jobOfferEntity : pageEntity.getJobOffers()){
+            tempDocumentsList.add(jobOfferEntity.getDocument());
+        }
+        return tempDocumentsList;
+    }
+
+    public void initializeCategory(){
+        categoryDropBox = new ComboBox<>();
+        categoryDropBox.setItems("Fahrdienst", "Werkstatt", "Verwaltung");
+        categoryDropBox.setPlaceholder("Kategorie");
+        categoryDropBox.addValueChangeListener( event -> {
+            if(event.getValue() == null){
+                errorContainer.setText("Sie haben nichts ausgewählt");
+                toAdd.setEnabled(false);
+                isCategoryFilled = false;
+            }else{
+                jobOfferEntity.setCategory(event.getValue());
+                isCategoryFilled = true;
+            }
+            toAdd.setEnabled(checkFilled());
+        });
+    }
+
+    public void initializeType(){
+        typeDropBox = new ComboBox<>();
+        typeDropBox.setItems("Vollzeit", "Teilzeit", "Ausbildung", "Praktikum");
+        typeDropBox.setPlaceholder("Stellenart");
+        typeDropBox.addValueChangeListener( event -> {
+            if(event.getValue() == null){
+                errorContainer.setText("Sie haben nichts ausgewählt");
+                toAdd.setEnabled(false);
+                isTypeFilled = false;
+            }else{
+                jobOfferEntity.setType(event.getValue());
+                isTypeFilled = true;
+            }
+            toAdd.setEnabled(checkFilled());
+        });
+    }
+
+    public void initializeDescription(){
+        description = new TextField();
+        description.setPlaceholder("Bezeichnung");
+        description.addValueChangeListener( event -> {
+            if(event.getValue() == null){
+                errorContainer.setText("Sie haben nichts ausgewählt");
+                toAdd.setEnabled(false);
+                isDescriptionFilled = false;
+            }else{
+                jobOfferEntity.setTitle(event.getValue());
+                isDescriptionFilled = true;
+            }
+            toAdd.setEnabled(checkFilled());
+        });
+    }
+
+    public void initializeLocation() {
+        location = new TextField();
+        location.setPlaceholder("Arbeitsort");
+        location.addValueChangeListener( event -> {
+            if(event.getValue() == null){
+                errorContainer.setText("Sie haben nichts ausgewählt");
+                toAdd.setEnabled(false);
+                isLocationFilled = false;
+            }else{
+                jobOfferEntity.setLocation(event.getValue());
+                isLocationFilled = true;
+            }
+            toAdd.setEnabled(checkFilled());
+        });
+    }
+
+    public boolean checkFilled(){
+        return isLocationFilled && isTypeFilled && isCategoryFilled && isDescriptionFilled && pdfsManager.isPdfUploaded();
+    }
+
+    public void readJobOffer(){
+        jobOfferEntity.setUser(userEntity);
+        jobOfferEntity.setPage(pageEntity);
+        jobOfferEntity.setDocument(documentService.findDocumentById(pdfsManager.getDocumentOfJobOfferId()));
+    }
+
+    public void initializeAddAndClearButtons(){
+        toAdd = new Button("Hinzufügen", event -> {
+            readJobOffer();
+            jobOfferService.save(jobOfferEntity);
+            UI.getCurrent().getPage().reload();
+        });
+        toAdd.setEnabled(false);
+        toAdd.setId("addButton");
+
+        toDelete = new Button("Eingabe Löschen", event -> {
+            description.clear();
+            location.clear();
+            typeDropBox.clear();
+            categoryDropBox.clear();
+            toAdd.setEnabled(false);
+            jobOfferEntity = new JobOfferEntity();
+        });
+
+        addClearDiv = new Div();
+        addClearDiv.add(toAdd,toDelete);
+        addClearDiv.setId("addClearDiv");
     }
 
 }
