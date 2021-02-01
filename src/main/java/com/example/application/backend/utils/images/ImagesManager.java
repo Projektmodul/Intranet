d207cb99326b31bd9941eaeb07dd0de3bdbe00cd
JessicaReistel <59623341+JessicaReistel@users.noreply.github.com>
2021-02-01 17:58:59 +0100

    Update ImagesManager.java
    version angepasst

--- src/main/java/com/example/application/backend/utils/images/ImagesManager.java
+++ src/main/java/com/example/application/backend/utils/images/ImagesManager.java
@@ -14,7 +14,7 @@ import java.util.List;
  * This class manages the images list and the imageManagers.
  *
  * @author  Anastasiya Jackwerth, Sabrine Gamdou, Jessica Reistel, Monika Martius
- * @version 2.0
+ * @version 3.0
  * @since   21-12-2020
  * @lastUpdated 01.02.2021 from Jessica Reistel, Monika Martius
  */
