cab9dc42079522aef145fa8f6862febfb48b2b7e
Litharshi <lsivarasa@stud.hs-bremen.de>
2021-02-01 10:36:25 +0100

    Tried to edit CareerView

--- src/main/java/com/example/application/backend/utils/GridJobOffer.java
+++ src/main/java/com/example/application/backend/utils/GridJobOffer.java
@@ -1,8 +1,11 @@
 package com.example.application.backend.utils;
 
+import com.example.application.backend.entities.DocumentEntity;
 import com.example.application.backend.entities.JobOfferEntity;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.server.StreamResource;
+import sun.jvm.hotspot.debugger.DebuggerUtilities;
 
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
@@ -10,27 +13,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GridJobOffer {
-    private String filename;
-    private String keyword;
-    private Anchor downloadLink;
     private  String title;
     private String location;
     private String type;
     private String category;
+    private DocumentEntity documentEntity;
+    private GridDocument gridDocument;
 
-    public GridJobOffer(String filename, String keyword, Anchor downloadLink, String title, String location, String type, String category) {
-        this.filename = filename;
-        this.keyword = keyword;
-        this.downloadLink = downloadLink;
+    public GridJobOffer(GridDocument gridDocument, DocumentEntity documentEntity, String title, String location, String type, String category) {
+        this.gridDocument = gridDocument;
+        this.documentEntity = documentEntity;
         this.category = category;
         this.location = location;
         this.type = type;
         this.title = title;
     }
 
-    public String getKeyword() {
-        return keyword;
-    }
     public String getTitle() {
         return title;
     }
@@ -43,11 +41,10 @@ public class GridJobOffer {
     public String getCategory() {
         return category;
     }
-    public String getFilename(){return filename;}
 
     public Anchor getDownloadLink() {
-        downloadLink.setTarget("_blank");
-        return downloadLink;
+        gridDocument.getDownloadLink().setTarget("_blank");
+        return gridDocument.getDownloadLink();
     }
 
     public static class JobOfferEntitiesConverter {
@@ -67,7 +64,7 @@ public class GridJobOffer {
         public static List<GridJobOffer> convertJobOfferEntities(List<JobOfferEntity> jobOfferEntities) {
             List<GridJobOffer> gridJobOffers = new ArrayList<>();
             for (JobOfferEntity jobOfferEntity : jobOfferEntities) {
-                GridJobOffer gridJobOffer = new GridJobOffer(jobOfferEntity.getFileName(), jobOfferEntity.getKeyword(),
+                GridJobOffer gridJobOffer = new GridJobOffer(documentEntity.getFileName(),jobOfferEntity.getKeyword(),
                         createDownloadLink(jobOfferEntity.getPath(),jobOfferEntity.getFileName()),
                         jobOfferEntity.getCategory(),jobOfferEntity.getLocation(),
                                 jobOfferEntity.getType(),jobOfferEntity.getTitle());
