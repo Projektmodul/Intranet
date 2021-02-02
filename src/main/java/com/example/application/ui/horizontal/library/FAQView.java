63608fd092583df9449230438e266c9c47c7a29d
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-02-02 14:17:53 +0100

    FAQView: Kommentare hinzugefügt und Code aufgeräumt

--- src/main/java/com/example/application/ui/horizontal/library/FAQView.java
+++ src/main/java/com/example/application/ui/horizontal/library/FAQView.java
@@ -1,6 +1,5 @@
 package com.example.application.ui.horizontal.library;
 
-import com.example.application.backend.entities.LinkEntity;
 import com.example.application.backend.entities.PageEntity;
 import com.example.application.backend.services.links.LinkService;
 import com.example.application.backend.services.pages.PageService;
@@ -8,16 +7,12 @@ import com.example.application.ui.MainView;
 import com.example.application.ui.auxiliary.InitData;
 import com.vaadin.componentfactory.Breadcrumb;
 import com.vaadin.componentfactory.Breadcrumbs;
-import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 /**
- *  FAQ View shows ...
+ *  FAQ View shows an overview of the most frequently asked questions
  *
  *  @author Monika Martius, Jessica Reistel, Laura Neuendorf
  *  @version 5.0
@@ -27,19 +22,11 @@ import com.vaadin.flow.router.Route;
 @Route(value = "fAQ", layout = MainView.class)
 @PageTitle("FAQ")
 public class FAQView extends Div {
-    private PageService pageService;
-    private PageEntity pageEntity;
-    private LinkService linkService;
-    
-
 
     public FAQView(PageService pageService, LinkService linkService) {
         setId("fAQ");
         setClassName("pageContentPosition");
         addClassName("libraryColorscheme");
-
-        this.pageService = pageService;
-        this.linkService = linkService;
 
         Breadcrumbs breadcrumbs = new Breadcrumbs();
         breadcrumbs.add(new Breadcrumb("Home"), new Breadcrumb("Bibliothek"), new Breadcrumb("FAQ"));
