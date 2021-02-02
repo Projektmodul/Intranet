ee074c872dda78553ea3ddf045b33e81476c25d9
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-02-02 15:25:10 +0100

    Rechtschreibfehler verbessert

--- src/main/java/com/example/application/ui/horizontal/library/MediaView.java
+++ src/main/java/com/example/application/ui/horizontal/library/MediaView.java
@@ -43,7 +43,7 @@ public class MediaView extends Div{
     }
 
     /**
-     * This method etches the data from the database
+     * This method fetches the data from the database
      * and displays it on the corresponding page
      */
     public void setContent(){
