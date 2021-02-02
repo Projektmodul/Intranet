eeaecc53879e057c2737c080bdc101d9c18437bc
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-02-02 18:23:33 +0100

    Added a find method for the news.

--- src/main/java/com/example/application/backend/repositories/NewsRepository.java
+++ src/main/java/com/example/application/backend/repositories/NewsRepository.java
@@ -4,4 +4,5 @@ import com.example.application.backend.entities.NewsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NewsRepository extends JpaRepository<NewsEntity, Integer> {
+    NewsEntity findByNewsId(int newsId);
 }
