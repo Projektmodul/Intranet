2a7eb2ecefe565d5799285d158fa298b024c72ef
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2021-02-02 18:33:47 +0100

    Updated header comment

--- src/main/java/com/example/application/backend/entities/NewsEntity.java
+++ src/main/java/com/example/application/backend/entities/NewsEntity.java
@@ -10,9 +10,9 @@ import java.util.List;
  * This is a basic news class.
  *
  * @author  Sabrine Gamdou
- * @version 1.0
+ * @version 2.0
  * @since   05.01.2020
- * @lastUpdated 05.01.2021
+ * @lastUpdated 02.02.2021 by Anastasiya Jackwerth and Sabrine Gamdou
  */
 
 @Entity(name ="news")
