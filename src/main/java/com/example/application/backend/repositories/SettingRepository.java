d77df62f69c88411c22a8002cc562457409db241
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-02-03 10:42:11 +0100

    Die Klasse SettingService hinzugefügt Bei Setting Entity die nicht benötigten Columns auskommentiert und die column boolean darkmode hinzugefügt

--- src/main/java/com/example/application/backend/repositories/SettingRepository.java
+++ src/main/java/com/example/application/backend/repositories/SettingRepository.java
@@ -4,4 +4,5 @@ import com.example.application.backend.entities.SettingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SettingRepository extends JpaRepository<SettingEntity, Integer> {
+    SettingEntity findById(int settingsId);
 }
---
+++ src/main/java/com/example/application/backend/services/settings/SettingService.java
@@ -0,0 +1,38 @@
+package com.example.application.backend.services.settings;
+
+import com.example.application.backend.entities.SettingEntity;
+import com.example.application.backend.repositories.SettingRepository;
+import com.vaadin.componentfactory.ToggleButton;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * This class is service class for the entity setting.
+ * The service layer processes requests from the UI layer.
+ *
+ * @author  Laura Neuendorf,
+ * @version 1.0
+ * @since   03.02.2021
+ * @lastUpdated
+ */
+
+@Service
+public class SettingService {
+
+    private final SettingRepository settingRepository;
+
+    @Autowired
+    public SettingService(SettingRepository settingRepository){
+        this.settingRepository = settingRepository;
+    }
+
+    public SettingRepository getSettingRepository(){
+        return settingRepository;
+    }
+
+    public void update(SettingEntity settingEntity, ToggleButton toggleButton){
+        settingEntity.setDarkmode(toggleButton.getValue());
+        getSettingRepository().saveAndFlush(settingEntity);
+    }
+
+}
