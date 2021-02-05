546180732f8caeed072b4377619d4ac6318bd268
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-02-05 14:39:11 +0100

    Kommentar SettingRepository

--- src/main/java/com/example/application/backend/repositories/SettingRepository.java
+++ src/main/java/com/example/application/backend/repositories/SettingRepository.java
@@ -3,6 +3,17 @@ package com.example.application.backend.repositories;
 import com.example.application.backend.entities.SettingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ *
+ * This interface is a repository for the entity setting.
+ * Each entity has a repository to manage its data exchange.
+ * The repository takes over the DAO functionality.
+ * @author  Laura Neuendorf
+ * @version 1.0
+ * @since   26.01.2021
+ * @lastUpdated 03.02.2021 by Laura Neuendorf
+ */
+
 public interface SettingRepository extends JpaRepository<SettingEntity, Integer> {
     SettingEntity findById(int settingsId);
 }
