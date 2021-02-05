28ce6d89a116804c1dc4c70a5efa630943d8371b
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-02-05 14:35:10 +0100

    Überflüssigen Code gelöscht in SettingEntity

--- src/main/java/com/example/application/backend/entities/SettingEntity.java
+++ src/main/java/com/example/application/backend/entities/SettingEntity.java
@@ -7,9 +7,9 @@ import javax.persistence.*;
  * This is a basic setting class.
  *
  * @author  Jessica Reistel, Laura Neuendorf and Sabrine Gamdou
- * @version 3.0
+ * @version 4.0
  * @since   21-12-2020
- * @lastUpdated 05.01.2021
+ * @lastUpdated 05.02.2021 by Laura Neuendorf
  */
 
 @Entity(name ="settings")
@@ -21,17 +21,6 @@ public class SettingEntity {
     private int settingsId;
 
     private boolean darkmode;
-    //private String type;
-    //private String category;
-
-   /* @Column(name ="font_size")
-    private String fontSize;
-
-    @Column(name ="font_type")
-    private String frontType;
-
-    @OneToOne(mappedBy = "settings")
-    private UsersEntity user;*/
 
     public int getSettingsId() {
         return settingsId;
@@ -48,37 +37,4 @@ public class SettingEntity {
     public void setDarkmode(boolean darkmode) {
         this.darkmode = darkmode;
     }
-
-   /* public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getFontSize() {
-        return fontSize;
-    }
-
-    public void setFontSize(String fontSize) {
-        this.fontSize = fontSize;
-    }
-
-    public String getFrontType() {
-        return frontType;
-    }
-
-    public void setFrontType(String frontType) {
-        this.frontType = frontType;
-    }*/
-
 }
