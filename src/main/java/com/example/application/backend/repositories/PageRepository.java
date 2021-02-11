deb445ea1d396f46edc85a2423446525ea7721d4
Anastasiya Jackwerth <ajackwerth@stud.hs-bremen.de>
2021-02-11 12:08:31 +0100

    Added Header comment

--- src/main/java/com/example/application/backend/repositories/PageRepository.java
+++ src/main/java/com/example/application/backend/repositories/PageRepository.java
@@ -3,6 +3,13 @@ package com.example.application.backend.repositories;
 import com.example.application.backend.entities.PageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface is a repository for the PageEntity.
+ * @author  Sabrine Gamdou
+ * @version 1.0
+ * @since   04.01.2021
+ * @lastUpdated 25.01.2021
+ */
 public interface PageRepository extends JpaRepository<PageEntity, Integer> {
     PageEntity findByPageId(int pageId);
 }
