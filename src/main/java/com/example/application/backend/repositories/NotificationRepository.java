78f40bc8d629a47e5e34a259bf38c2dc70fca26b
Anastasiya Jackwerth <ajackwerth@stud.hs-bremen.de>
2021-02-11 12:08:25 +0100

    Added Header comment

--- src/main/java/com/example/application/backend/repositories/NotificationRepository.java
+++ src/main/java/com/example/application/backend/repositories/NotificationRepository.java
@@ -1,11 +1,15 @@
 package com.example.application.backend.repositories;
 
 import com.example.application.backend.entities.NotificationEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface is a repository for the NotificationEntity.
+ * @author  Sabrine Gamdou
+ * @version 1.0
+ * @since   04.01.2021
+ * @lastUpdated 25.01.2021
+ */
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Integer> {
-    Page<NotificationEntity> findBy(Pageable pageable);
     NotificationEntity findByNotificationId(int notificationId);
 }
