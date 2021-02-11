2715a3af71abf20f19b845723bf7e5b60591818a
Anastasiya Jackwerth <ajackwerth@stud.hs-bremen.de>
2021-02-11 12:08:01 +0100

    Added Header comment

--- src/main/java/com/example/application/backend/repositories/DocumentRepository.java
+++ src/main/java/com/example/application/backend/repositories/DocumentRepository.java
@@ -3,6 +3,13 @@ package com.example.application.backend.repositories;
 import com.example.application.backend.entities.DocumentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface is a repository for the DocumentEntity.
+ * @author  Sabrine Gamdou
+ * @version 1.0
+ * @since   04.01.2021
+ * @lastUpdated 25.01.2021
+ */
 public interface DocumentRepository extends JpaRepository<DocumentEntity, Integer> {
     DocumentEntity findByDocumentId(int documentId);
 }
