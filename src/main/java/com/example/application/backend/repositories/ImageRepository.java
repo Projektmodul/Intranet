d83e466f0ad45300a6d3fda258537f85e1e362b3
Anastasiya Jackwerth <ajackwerth@stud.hs-bremen.de>
2021-02-11 12:08:10 +0100

    Added Header comment

--- src/main/java/com/example/application/backend/repositories/ImageRepository.java
+++ src/main/java/com/example/application/backend/repositories/ImageRepository.java
@@ -3,6 +3,13 @@ package com.example.application.backend.repositories;
 import com.example.application.backend.entities.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface is a repository for the ImageEntity.
+ * @author  Sabrine Gamdou
+ * @version 1.0
+ * @since   04.01.2021
+ * @lastUpdated 25.01.2021
+ */
 public interface ImageRepository extends JpaRepository<ImageEntity, Integer> {
     ImageEntity findByImageId(int imageId);
 }
