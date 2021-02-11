129d9103c61d729d51c055ddbc53fd43005d9062
Anastasiya Jackwerth <ajackwerth@stud.hs-bremen.de>
2021-02-11 11:48:20 +0100

    Updated header comment and deleted unnecessary code.

--- src/main/java/com/example/application/backend/utils/images/ImageViewer.java
+++ src/main/java/com/example/application/backend/utils/images/ImageViewer.java
@@ -10,22 +10,19 @@ import com.vaadin.flow.server.StreamResource;
  *
  * @author  Anastasiya Jackwerth, Sabrine Gamdou
  * @version 3.0
- * @since   21-12-2020
+ * @since   21.12.2020
  * @lastUpdated 19.01.2021 from Anastasiya Jackwerth, Sabrine Gamdou
  */
-
 @Tag("object")
 public class ImageViewer extends Component implements HasSize {
 
-    public ImageViewer(StreamResource resource) {
+    public ImageViewer(StreamResource resource){
         this();
         getElement().setAttribute("data", resource);
     }
 
-    protected ImageViewer() {
+    protected ImageViewer(){
         getElement().setAttribute("type", "image/jpeg");
         setSizeFull();
     }
-
-
 }
