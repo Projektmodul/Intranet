66283e825793380ba3ab5b9b7114d74b6a2b54bd
Anastasiya Jackwerth <ajackwerth@stud.hs-bremen.de>
2021-02-11 11:53:11 +0100

    Added method comments and deleted unnecessary code.

--- src/main/java/com/example/application/backend/services/notifications/NotificationDataProvider.java
+++ src/main/java/com/example/application/backend/services/notifications/NotificationDataProvider.java
@@ -27,8 +27,6 @@ import java.util.Map;
  * @since 06.01.2021
  * @lastUpdated 26.01.2021 from Anastasiya Jackwerth, Sabrine Gamdou
  */
-
-
 public class NotificationDataProvider {
 
     private Map<Details, Checkbox> notificationsMap;
@@ -38,7 +36,7 @@ public class NotificationDataProvider {
 
     private int notificationCounter;
 
-    public NotificationDataProvider() {
+    public NotificationDataProvider(){
 
         notificationsMap = new HashMap<>();
         checkedNotifications = new ArrayList<>();
@@ -48,13 +46,17 @@ public class NotificationDataProvider {
 
     }
 
-    public void findNotification(NotificationService notificationService) {
+    /**
+     * This method saves the notifications in a list and then creates notificationDetails that will be shown on
+     * the UI. Each notification is rendered into a Details-component
+     * @param notificationService this service manages the requests between the UI-Layer and the persistence-Layer
+     */
+    public void findNotification(NotificationService notificationService){
         notificationsMap.clear();
-        //Retrieve the entities from the database
+
         List<NotificationEntity> notifications = notificationService.findAll();
 
-        //Each notification is rendered into a Details-component
-        for (NotificationEntity entity : notifications) {
+        for (NotificationEntity entity : notifications){
             if (!entity.getStatus()) {
                 fillDetails(new Details(), entity);
                 notificationCounter++;
@@ -62,21 +64,26 @@ public class NotificationDataProvider {
         }
     }
 
-    public void fillDetails(Details notificationDetails, NotificationEntity notificationEntity) {
-        //Styling the Details and adding the notification data to it
+    /**
+     * This method creates a Details component from the notificationsEntities data and adds the Details to a Details-list
+     * @param notificationDetails this is where the notification data will be rendered
+     * @param notificationEntity is the entity to be rendered
+     */
+    public void fillDetails(Details notificationDetails, NotificationEntity notificationEntity){
         notificationDetails.setSummaryText(notificationEntity.getTitle());
         notificationDetails.addContent(new H5(notificationEntity.getDescription()), new Text(notificationEntity.getDate().toString()));
 
         notificationDetails.addThemeVariants(DetailsVariant.REVERSE, DetailsVariant.REVERSE);
 
-        //Add the Details to the Details-list
         notificationsMap.put(notificationDetails, setCheckboxes(notificationEntity));
-
     }
 
-    public void addNotifications(VerticalLayout allNotificationsContainer, NotificationService notificationService) {
-        //Add all Details and checkboxes to the notification container with their clickEvents
-        //Set styling classes for the layout and the checkboxes
+    /**
+     * This method adds all Details and checkboxes to the notification container with their clickEvents
+     * @param allNotificationsContainer this is where all notifications details and checkboxes are stored
+     * @param notificationService this service manages the requests between the UI-Layer and the persistence-Layer
+     */
+    public void addNotifications(VerticalLayout allNotificationsContainer, NotificationService notificationService){
         notificationsMap.forEach((details, checkbox) -> {
             checkbox.setClassName("checkbox");
             details.setId("singleNotificationDetails");
@@ -88,10 +95,9 @@ public class NotificationDataProvider {
         });
     }
 
-    public void addClickEventOnCheckbox(Checkbox checkbox, NotificationService notificationService) {
+    public void addClickEventOnCheckbox(Checkbox checkbox, NotificationService notificationService){
         checkbox.addValueChangeListener(event -> {
-            if (event.getValue()) {
-
+            if (event.getValue()){
                 changeNotificationStatus(notificationService, checkbox);
             } else {
                 uncheckNotification(notificationService, checkbox);
@@ -99,18 +105,23 @@ public class NotificationDataProvider {
         });
     }
 
-    public int findNotificationIndex(NotificationEntity notificationEntity) {
-        for (int i = 0; i < checkedNotifications.size(); i++) {
-            if (checkedNotifications.get(i).getNotificationId() == (notificationEntity.getNotificationId())) {
+    /**
+     * This method fetches the ids of the checked notifications and compares them with the current notificationEntity
+     * @param notificationEntity is the entity to be compared
+     * @return int returns the id matching the id of the notificationsEntity
+     */
+    public int findNotificationIndex(NotificationEntity notificationEntity){
+        for (int i = 0; i < checkedNotifications.size(); i++){
+            if (checkedNotifications.get(i).getNotificationId() == (notificationEntity.getNotificationId())){
                 return i;
             }
         }
         return -1;
     }
 
-    public void setClickEventOnSaveButton(NativeButton saveButton, NotificationService notificationService) {
+    public void setClickEventOnSaveButton(NativeButton saveButton, NotificationService notificationService){
         saveButton.addClickListener(e -> {
-            for(NotificationEntity notification : checkedNotifications) {
+            for(NotificationEntity notification : checkedNotifications){
                 notificationService.save(notification);
             }
             initiator.notificationCounterChanged();
@@ -118,36 +129,40 @@ public class NotificationDataProvider {
         });
     }
 
-    public void uncheckNotification(NotificationService notificationService, Checkbox checkbox) {
+    public void uncheckNotification(NotificationService notificationService, Checkbox checkbox){
         NotificationEntity notificationEntity = getNotification(notificationService, readIdFromCheckbox(checkbox));
         notificationEntity.setStatus(checkbox.getValue());
         checkedNotifications.remove(checkedNotifications.get(findNotificationIndex(notificationEntity)));
     }
 
-    public void changeNotificationStatus(NotificationService notificationService, Checkbox checkbox) {
+    public void changeNotificationStatus(NotificationService notificationService, Checkbox checkbox){
         NotificationEntity notificationEntity = getNotification(notificationService, readIdFromCheckbox(checkbox));
         notificationEntity.setStatus(checkbox.getValue());
         checkedNotifications.add(notificationEntity);
     }
 
-    public int readIdFromCheckbox(Checkbox checkbox) {
+    public int readIdFromCheckbox(Checkbox checkbox){
         return Integer.parseInt(checkbox.getLabel());
     }
 
-    public NotificationEntity getNotification(NotificationService notificationService, int notificationId) {
+    public NotificationEntity getNotification(NotificationService notificationService, int notificationId){
         return notificationService.findById(notificationId);
     }
 
-    public Checkbox setCheckboxes(NotificationEntity notificationEntity) {
-        //write the id and the status of the notification to the checkbox
+    /**
+     * This method writes the id and the status of the notification to the checkbox
+     * @param notificationEntity is the entity to be rendered
+     * @return Checkbox returns the notifications Checkbox
+     */
+    public Checkbox setCheckboxes(NotificationEntity notificationEntity){
         return new Checkbox(Integer.toString(notificationEntity.getNotificationId()), notificationEntity.getStatus());
     }
 
-    public int getNotificationCounter() {
+    public int getNotificationCounter(){
         return notificationCounter;
     }
 
-    public NotificationCounterChangedInitiator getInitiator() {
+    public NotificationCounterChangedInitiator getInitiator(){
         return initiator;
     }
 }
