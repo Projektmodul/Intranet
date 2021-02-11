53022e3c6004b0fc30fb439b7fa66b13d5d8f256
Anastasiya Jackwerth <ajackwerth@stud.hs-bremen.de>
2021-02-11 11:54:48 +0100

    Added Header comment and deleted unnecessary code.

--- src/main/java/com/example/application/backend/services/notifications/NotificationService.java
+++ src/main/java/com/example/application/backend/services/notifications/NotificationService.java
@@ -3,12 +3,18 @@ package com.example.application.backend.services.notifications;
 import com.example.application.backend.entities.NotificationEntity;
 import com.example.application.backend.repositories.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * A service class for the NotificationEntities
+ *
+ * @author Sabrine Gamdou, Anastasiya Jackwerth
+ * @version 2.0
+ * @since 11.01.2021
+ * @lastUpdated 11.02.2021 by Sabrine Gamdou, Anastasiya Jackwerth
+ */
 @Service
 public class NotificationService {
 
@@ -25,10 +31,6 @@ public class NotificationService {
 
     public List<NotificationEntity> findAll(){
         return getNotificationRepository().findAll();
-    }
-
-    public Page<NotificationEntity> find(Pageable pageable) {
-        return getNotificationRepository().findBy(pageable);
     }
 
     public void save(NotificationEntity notificationEntity) {
