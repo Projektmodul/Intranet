667d0e287739d9dfb4dc80b6a85c4644a409fb06
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-02-12 10:26:12 +0100

    Blockkommentar bei JobOfferRepository hinzugefügt,

--- src/main/java/com/example/application/backend/repositories/JobOfferRepository.java
+++ src/main/java/com/example/application/backend/repositories/JobOfferRepository.java
@@ -3,6 +3,13 @@ package com.example.application.backend.repositories;
 import com.example.application.backend.entities.JobOfferEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface is a repository for the JobOfferEntity.
+ * @author Sabrine Gamdou
+ * @version 2.0
+ * @since 05.01.2021
+ * @lastUpdatet 31.01.2021 by Litharshiga Sivarasa
+ */
 public interface JobOfferRepository extends JpaRepository<JobOfferEntity, Integer> {
     JobOfferEntity findByJobOfferId(int jobOfferId);
 
