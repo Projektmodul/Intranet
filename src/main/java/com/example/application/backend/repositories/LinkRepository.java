564c5415c305fdab3cc041b3f2bef8efa8c74df7
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-02-12 10:28:28 +0100

    Blockkommentar bei LinkRepository hinzugefügt,

--- src/main/java/com/example/application/backend/repositories/LinkRepository.java
+++ src/main/java/com/example/application/backend/repositories/LinkRepository.java
@@ -3,6 +3,13 @@ package com.example.application.backend.repositories;
 import com.example.application.backend.entities.LinkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface is a repository for the LinkEntity.
+ * @author Sabrine Gamdou
+ * @version 2.0
+ * @since 05.01.2021
+ * @lastUpdatet 26.01.2021 by Laura Neuendorf
+ */
 public interface LinkRepository extends JpaRepository<LinkEntity, Integer> {
     LinkEntity findByLinkId(int linkId);
 }
