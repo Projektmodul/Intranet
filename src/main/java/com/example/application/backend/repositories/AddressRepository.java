20e4ea052d92d8a9f0b407c5d434a7c318fcacc2
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-02-12 10:18:41 +0100

    Blockkommentar bei AddressRepository hinzugefügt, Schreibfehler bei AddressService (Zeile 25) verbessert

--- src/main/java/com/example/application/backend/repositories/AddressRepository.java
+++ src/main/java/com/example/application/backend/repositories/AddressRepository.java
@@ -3,6 +3,12 @@ package com.example.application.backend.repositories;
 import com.example.application.backend.entities.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface is a repository for the AddressEntity.
+ * @author Sabrine Gamdou
+ * @version 2.0
+ * @since 05.01.2021
+ * @lastUpdatet 12.01.2021 by Jessica Reistel
+ */
 public interface AddressRepository extends JpaRepository<AddressEntity, Integer> {
-    AddressEntity findByAddressId(int addressId);
 }
