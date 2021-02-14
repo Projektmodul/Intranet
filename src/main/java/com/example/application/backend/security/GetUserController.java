3863b4d2178b0086e346955dca44e5d0b0606aa2
Vanessa Skowronsky <vskowronsky@stud.hs-bremen.de>
2021-02-14 17:27:21 +0100

    Added block comment to GetUserController.java

--- src/main/java/com/example/application/backend/security/GetUserController.java
+++ src/main/java/com/example/application/backend/security/GetUserController.java
@@ -1,17 +1,19 @@
-/*
-   @author Monika Martius
- *  @version 1.0
- *  @since 23.01.2021
- *  @lastUpdated 23.01.2021
- */
-
 package com.example.application.backend.security;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
+/**
+ * This class returns the current logged in user from Spring Security
+ *
+ * @author  Monika Martius
+ * @version 1.0
+ * @since   23.01.2021
+ * @lastUpdated 23.01.2021
+ */
 public class GetUserController {
     Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
     public String getUsername(){
         String username;
         if(principal instanceof UserDetails){
