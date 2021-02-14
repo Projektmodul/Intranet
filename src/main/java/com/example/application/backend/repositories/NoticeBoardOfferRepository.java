78909e8ced4c82543da6445e32240ea77703d2f4
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-02-14 09:24:26 +0100

    Blockkommentar bei NoticeBoardOfferRepository hinzugefügt

--- src/main/java/com/example/application/backend/repositories/NoticeBoardOfferRepository.java
+++ src/main/java/com/example/application/backend/repositories/NoticeBoardOfferRepository.java
@@ -1,16 +1,15 @@
-/**
- *  Home View shows ...
- *
- *  @author Monika Martius, Jessica Reistel
- *  @version 1.0
- *  @since 23.01.2021
- *  @lastUpdated 23.01.2021
- */
 package com.example.application.backend.repositories;
 
 import com.example.application.backend.entities.NoticeBoardOfferEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface is a repository for the NoticeBoardOfferEntity.
+ * @author Sabrine Gamdou
+ * @version 2.0
+ * @since 05.01.2021
+ * @lastUpdatet 24.01.2021 by Jessica Reistel and Monika Martius
+ */
 public interface NoticeBoardOfferRepository extends JpaRepository<NoticeBoardOfferEntity, Integer> {
     NoticeBoardOfferEntity findByNoticeBoardOfferId(int noticeBoardOfferId);
 
