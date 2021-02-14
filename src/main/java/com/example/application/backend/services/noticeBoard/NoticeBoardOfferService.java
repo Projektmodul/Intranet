9419e988091086ac8d17b22203410f594d2baeea
Laura Neuendorf <lschuppenhauer@stud.hs-bremen.de>
2021-02-14 09:44:56 +0100

    Blockkommentar bei NoticeBoardOfferService hinzugefügt

--- src/main/java/com/example/application/backend/services/noticeBoard/NoticeBoardOfferService.java
+++ src/main/java/com/example/application/backend/services/noticeBoard/NoticeBoardOfferService.java
@@ -1,19 +1,19 @@
-/**
- *  Home View shows ...
- *
- *  @author Monika Martius, Jessica Reistel
- *  @version 1.0
- *  @since 23.01.2021
- *  @lastUpdated 23.01.2021
- */
-
 package com.example.application.backend.services.noticeBoard;
+
 import com.example.application.backend.entities.NoticeBoardOfferEntity;
 import com.example.application.backend.repositories.NoticeBoardOfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+/**
+ * This class is service class for the entity notice board offer.
+ * The service layer processes requests from the UI layer.
+ *
+ * @author  Monika Martius and Jessica Reistel
+ * @version 1.0
+ * @since   24.01.2021
+ */
 @Service
 public class NoticeBoardOfferService {
 
@@ -32,6 +32,12 @@ public class NoticeBoardOfferService {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    /**
+     * This method selects data from a table by using Spring JdbcTemplate.
+     * The query searches for the highest notice board offer id.
+     * @return int as max id or null
+     */
     public int findMaxId() {
         String sql = "SELECT max(notice_board_offer_id) from notice_board_offers";
         try {
